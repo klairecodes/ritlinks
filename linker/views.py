f6c1d490ce520fed0cee2d4368d677a7920d7187
[klaus] <[kcurde@gmail.com]>
2020-09-05 17:37:28 -0400

    added non-functioning templates, css, and html

--- linker/views.py
+++ linker/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from linker.models import LinkEntry, Category
 
 # Create your views here.
+
+def index(request):
+    num_links = LinkEntry.objects.all().count()
+
+    context = {
+        'num_links' : num_links,
+    }
+
+    # Render the HTML template
+    return render(request, 'index.html', context=context)
--- ritlinks/settings.py
+++ ritlinks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'ritlinks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
