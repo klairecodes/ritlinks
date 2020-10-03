e69f9284245ed1497d39c84bdc97123eb92aa0dd
Klaus Curde <kcurde@gmail.com>
2020-10-03 19:52:23 -0400

    can successfully loop through data stored in models, and can see context variables in views

--- linker/views.py
+++ linker/views.py
@@ -5,9 +5,11 @@ from linker.models import LinkEntry, Category
 
 def index(request):
     num_links = LinkEntry.objects.all().count()
+    all_links = LinkEntry.objects.all()
 
     context = {
         'num_links' : num_links,
+        'all_links' : all_links,
     }
 
     # Render the HTML template
