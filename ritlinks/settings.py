bba20db9fb967e8a62ff18e4fb30b1b7d57dd02a
Klaus Curde <kcurde@gmail.com>
2021-02-23 00:52:17 -0500

    added wildcard for allowed hosts

--- ritlinks/settings.py
+++ ritlinks/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '-!oj1rxe@y2obrq=2x6$et4s4lja+-@qraku8
 # DEBUG looks at an os environment variable
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["0.0.0.0", "ritlinks-ritlinks.apps.okd4.csh.rit.edu", "ritlinks.cs.house", "ritlinks.com"]
+ALLOWED_HOSTS = [".csh.rit.edu", ".cs.house", "ritlinks.com"]
 
 
 # Application definition
