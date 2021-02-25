bf5608bb6b32aef50e808e1e3f582703dd9de840
Klaus Curde <kcurde@gmail.com>
2021-02-25 02:48:09 -0500

    design overhaul

--- linker/views.py
+++ linker/views.py
@@ -7,7 +7,7 @@ def index(request):
     num_links = LinkEntry.objects.all().count()
     all_categories = Category.objects.all()
     all_links = LinkEntry.objects.all()
-    top_message = "A collection of all of RIT's numerous websites."
+    top_message = "A collection of RIT's numerous websites."
 
     context = {
         'num_links' : num_links,
