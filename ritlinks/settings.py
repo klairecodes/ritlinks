ea614741607227cc76854c4ca2c008e0e148dee7
Klaus Curde <kcurde@gmail.com>
2021-02-27 02:39:36 -0500

    allowed_hosts

--- ritlinks/settings.py
+++ ritlinks/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '-!oj1rxe@y2obrq=2x6$et4s4lja+-@qraku8
 # DEBUG looks at an os environment variable
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = [".csh.rit.edu", ".cs.house", "ritlinks.com", "localhost"]
+ALLOWED_HOSTS = ["0.0.0.0", "ritlinks.cs.house"] # VERY TEMPORARY
 
 
 # Application definition
