f59ae178609a95afcc988f391faf9fd1edc3b098
Klaus Curde <kcurde@gmail.com>
2021-07-26 00:01:08 -0400

    working local postgres db

--- ritlinks/settings.py
+++ ritlinks/settings.py
@@ -79,11 +79,30 @@ WSGI_APPLICATION = 'ritlinks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
+ 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ 
+        'NAME': 'ritlinks',
+ 
+        'USER': 'ritlinks-user',
+ 
+        'PASSWORD': 'ponytail-acrimony6',
+ 
+        'HOST': 'localhost',
+ 
+        'PORT': '5432',
+ 
     }
+ 
 }
 
 
